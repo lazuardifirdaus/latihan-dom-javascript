47e02ce6ff62296e45337e8e77ee5ec50e54baa5
lazuardifirdaus <lazuardi.firdaus88@gmail.com>
2024-06-10 16:18:28 +0700

    Mencari kesalahan kode #1

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const showResult = (n) => {
-    const numberInput = document.getElementsByClassName("number");
-    if (numberInput) {
-        
+const calculatingInput = (input_text_1, input_text_2) => {
+
+    let input_1 = document.getElementById(input_text_1);
+    let input_2 = document.getElementById(input_text_2);
+    
+    let firstNumber = parseInt(input_1.value);
+    let secondNumber = parseInt(input_2.value);
+
+    let sum = firstNumber + secondNumber;
+
+    showResult(firstNumber, secondNumber, sum);
+}
+
+const showResult = (firstNumber, secondNumber, sum) => {
+    if (sum % 3 === 0) {
+        document.writeln(`Fizz = ${firstNumber} + ${secondNumber}`);
+    } else if (sum % 5 === 0) {
+        document.writeln(`Buzz = ${firstNumber} + ${secondNumber}`);
+    } else if (sum % 3 === 0 && sum % 5 === 0) {
+        document.writeln(`FizzBuzz = ${firstNumber} + ${secondNumber}`);
     } else {
-        
+        document.writeln(`${firstNumber + secondNumber}`);
     }
 }
