8dec40f34edf8a3cb82f3e5e993d828056ea9959
lazuardifirdaus <lazuardi.firdaus88@gmail.com>
2024-06-10 18:18:26 +0700

    Selesai, dengan catatan #perlu_perbaikan

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const calculatingInput = (input_text_1, input_text_2) => {
-
+let calculatingInput = (input_text_1, input_text_2) => {
     let input_1 = document.getElementById(input_text_1);
     let input_2 = document.getElementById(input_text_2);
     
@@ -8,17 +7,34 @@ const calculatingInput = (input_text_1, input_text_2) => {
 
     let sum = firstNumber + secondNumber;
 
-    showResult(firstNumber, secondNumber, sum);
+    showResult(firstNumber, secondNumber, sum); // Panggil showResult di sini
 }
 
-const showResult = (firstNumber, secondNumber, sum) => {
-    if (sum % 3 === 0) {
-        document.writeln(`Fizz = ${firstNumber} + ${secondNumber}`);
-    } else if (sum % 5 === 0) {
-        document.writeln(`Buzz = ${firstNumber} + ${secondNumber}`);
+let showResult = (firstNumber, secondNumber, sum) => {
+    let resultContainer = document.getElementById('checkResult');
+    let resultText;
+
+    
+    if (sum === 0){
+        alert("Inputnya tidak valid!");
+        resultText = undefined;
     } else if (sum % 3 === 0 && sum % 5 === 0) {
-        document.writeln(`FizzBuzz = ${firstNumber} + ${secondNumber}`);
+        resultContainer.innerHTML += "<br />";
+        resultText = `FizzBuzz = ${firstNumber} + ${secondNumber}`;
+    } else if (sum % 3 === 0) {
+        resultContainer.innerHTML += "<br />";
+        resultText = `Fizz = ${firstNumber} + ${secondNumber}`;
+    } else if (sum % 5 === 0) {
+        resultContainer.innerHTML += "<br />";
+        resultText = `Buzz = ${firstNumber} + ${secondNumber}`;
     } else {
-        document.writeln(`${firstNumber + secondNumber}`);
+        resultContainer.innerHTML += "<br />";
+        resultText = `${firstNumber + secondNumber}`;
+    }
+
+    if (typeof resultText === 'undefined'){
+        resultContainer.innerHTML = ""; // Menggunakan innerHTML untuk menampilkan hasil
+    } else {
+        resultContainer.innerHTML += resultText + "<br />"; // Menggunakan innerHTML untuk menampilkan hasil
     }
 }
